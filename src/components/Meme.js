56865da3d2301b3f32eeb11fb1553270e57a5b54
joyjsmun <joy.js.mun@gmail.com>
2022-03-26 21:12:53 +0900

    added random func

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,16 @@
+import memesData from "../memesData";
+
 export default function Meme(){
+    const memes = memesData.data.memes;
+    const randNum = Math.floor(Math.random()*memes.length)
+    console.log(memes[randNum].url);
     return(
         <div className="meme">
-            <form>
-                    <input placeholder=""/>
-                    <input placeholder=""/>
-            </form>
-                <button>Get a new meme image</button>
+            <div className="form">
+                    <input placeholder="Top Text"/>
+                    <input placeholder="Bottom Text"/>
+                    <button>Get a new meme image</button>
+            </div>
         </div>
     )
 }
