36a7992f70def297e8fa2dfade9543e55be5fcf2
joyjsmun <joy.js.mun@gmail.com>
2022-03-26 19:54:27 +0900

    added header

---
+++ src/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return(
+        <nav>
+            <img className="nav--img" src="./images/troll-face.png" />
+            <span>Meme Generator</span>
+        </nav>
+    )
+}
