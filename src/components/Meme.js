bb7a4718d96ac73ae3af409d43d39fd5556569ea
joyjsmun <joy.js.mun@gmail.com>
2022-03-27 18:17:30 +0900

    added image state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,16 +1,23 @@
 import memesData from "../memesData";
+import React from "react";
+
+
 
 export default function Meme(){
-    const memes = memesData.data.memes;
-    const randNum = Math.floor(Math.random()*memes.length)
-    console.log(memes[randNum].url);
+    const [memeImage,setImage] = React.useState("")
+    const RandomMeme = () => {
+        const memes = memesData.data.memes;
+        const randNum = Math.floor(Math.random()*memes.length)
+        setImage(memes[randNum].url);
+    }
     return(
         <div className="meme">
             <div className="form">
                     <input placeholder="Top Text"/>
                     <input placeholder="Bottom Text"/>
-                    <button>Get a new meme image</button>
+                    <button onClick={RandomMeme}>Get a new meme image</button>
             </div>
+            {memeImage? <img className="memeImg" src={memeImage} alt="img" /> : null}
         </div>
     )
 }
